26c364e0e89b9149edb89935d62773fee178231d
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 09:31:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,5 @@ function destructivelyAppendKitten(name) {
 }
 
   
-  
-  
-  // describe('destructivelyAppendKitten(name)', function() {
-  //   it('appends a kitten to the end of the kittens array', function() {
-  //     destructivelyAppendKitten('Ralph')
-  //     expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
-  //   })
-  // })
+
+
