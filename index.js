2f90efa26ab1e44def82411202041c5f3b09d543
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 09:30:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  
+}
+
+  
+  
+  
+  describe('destructivelyAppendKitten(name)', function() {
+    it('appends a kitten to the end of the kittens array', function() {
+      destructivelyAppendKitten('Ralph')
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])
+    })
+  })
