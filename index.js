a3c54c8ed708b2aac81a4e66b9c8c83ca3118704
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-26 10:03:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,22 +28,9 @@ function removeLastKitten() {
   return kittens.slice(0, kittens.length -1)
 }
 
-
-
-
-
-//   describe('removeLastKitten()', function() {
-//     it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-//       expect(removeLastKitten()).toEqual(["Milo", "Otis"])
-
-//       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-//     })
-//   })
-
-
-
-
-
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
 
 //   describe('removeFirstKitten()', function() {
 //     it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
